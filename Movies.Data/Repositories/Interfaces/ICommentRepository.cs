f783aa1c1283b1b7f16b195fcef7e0961befd56e
joaom-artins <milanezijoao51@gmail.com>
2025-01-20 13:26:25 -0300

    Feat:Implementa generic Repository e cria outras repositories

---
+++ Movies.Data/Repositories/Interfaces/ICommentRepository.cs
@@ -0,0 +1,8 @@
+﻿using Movies.Core.Models;
+using Movies.Data.Repositories.Generic.Interfaces;
+
+namespace Movies.Data.Repositories.Interfaces;
+
+public interface ICommentRepository : IGenericRepository<CommentModel>
+{
+}
---
+++ Movies.Data/Repositories/Repository/CommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using Movies.Core.Models;
+using Movies.Data.Context;
+using Movies.Data.Repositories.Generic;
+using Movies.Data.Repositories.Interfaces;
+
+namespace Movies.Data.Repositories.Repository;
+
+public class CommentRepository(AppDbContext context) : GenericRepository<CommentModel>(context),
+    ICommentRepository
+{
+}
