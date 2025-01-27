23b2f704479dc304d180129abf81ab7a14c7676a
joaom-artins <milanezijoao51@gmail.com>
2025-01-27 20:05:10 -0300

    Feat:Implementa base do NotificationContext

---
+++ Movies.Common/Notification/NotificationContext.cs
@@ -0,0 +1,23 @@
+﻿using Movies.Common.Notification.Interfcaes;
+
+namespace Movies.Common.Notification;
+
+public class NotificationContext : INotificationContext
+{
+
+    private readonly List<NotificationMessage> _notifications = [];
+
+    public IReadOnlyCollection<NotificationMessage> Notifications => _notifications;
+
+    public bool HasNotifications => _notifications.Count > 0;
+
+    public void AddNotification(string message, string type)
+    {
+        _notifications.Add(new NotificationMessage(message, type));
+    }
+
+    public void AddNotifications(IEnumerable<NotificationMessage> notifications)
+    {
+        _notifications.AddRange(notifications);
+    }
+}
